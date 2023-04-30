3ea4a0c12e8b235f86b07dcd81ad07f3b1dec806
Rothin Roy <mr.rothinroy@gmail.com>
2023-04-30 17:46:21 +0600

    diposit balance added

---
+++ js/login.js
@@ -0,0 +1,14 @@
+document.getElementById('btn-submit').addEventListener('click', function(){
+  const emailField = document.getElementById('user-email');
+  const email = emailField.value;
+  
+  const passwordField = document.getElementById('user-password');
+  const password = passwordField.value;
+//   console.log(email, password);
+
+  if(email === 'sontan@baap.com' && password === 'secret'){
+    window.location.href = 'bank.html';
+  }else{
+    alert('toke ami tejjo sontan korlam')
+  }
+})
